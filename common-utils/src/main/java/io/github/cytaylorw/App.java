b1535457fbd8e965790104193bfcf2474e43e075
Taylor <cytaylorw@gmail.com>
2021-10-28 22:34:23 +0800

    backup

---
+++ common-utils/src/main/java/io/github/cytaylorw/App.java
@@ -0,0 +1,13 @@
+package io.github.cytaylorw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
